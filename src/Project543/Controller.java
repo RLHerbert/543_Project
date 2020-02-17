ad4d94fc0aef1068e33578f4d7f7841482351dc5
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-02-17 13:36:47 -0800

    Renamed package

--- src/Project543/Controller.java
+++ src/Project543/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package Project543;
 
 public class Controller {
 }
--- src/Project543/Main.java
+++ src/Project543/Main.java
@@ -1,12 +1,8 @@
-package sample;
+package Project543;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
