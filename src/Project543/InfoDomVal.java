e22a246328337030671eebe79255228f872c32d6
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-02-18 14:00:43 -0800

    Updates to InfoDomVal.java

--- src/Project543/InfoDomVal.java
+++ src/Project543/InfoDomVal.java
@@ -1,14 +1,17 @@
 package Project543;
 
+//A Class which holds an Information Domain Value (IDV) and relevant methods
 public class InfoDomVal {
 
+    //Enum for complexity
     public enum Complexity{
         SIMPLE, AVERGAGE, COMPLEX;
     }
 
+    //Member Variables
     public static int NUMWEIGHTS = 3;
 
-    private Complexity selectedComplexity;
+    private Complexity selectedComplexity; //The user selected complexity for the information domain value
     private int[] complexWeightFactors; //Complexity weight factors
     private int numOfDomVal, sumOfDomVal;
 
@@ -19,11 +22,35 @@ public class InfoDomVal {
         this.complexWeightFactors[1] = 4;
         this.complexWeightFactors[2] = 6;
         numOfDomVal = 0;
-        //sumOfDomVal = 0;
         this.setSumOfDomVal();
     }
 
-    public void setSumOfDomVal() {
-        this.sumOfDomVal = (this.numOfDomVal) * (this.selectedComplexity.ordinal());
+    public InfoDomVal(int[] complexWeightFactors){
+        this.selectedComplexity = Complexity.AVERGAGE;
+        this.complexWeightFactors = complexWeightFactors;
+        numOfDomVal = 0;
+        this.setSumOfDomVal();
     }
+
+    //Getters
+    public int getSumOfDomVal(){
+        return this.sumOfDomVal;
+    }
+
+    //Setters
+    public void setSumOfDomVal() {
+        this.sumOfDomVal = (this.numOfDomVal) * (complexWeightFactors[this.selectedComplexity.ordinal()]);
+    }
+
+    public void setNumOfDomVal(int numOfDomVal1){
+        this.numOfDomVal = numOfDomVal1;
+        setSumOfDomVal();
+    }
+
+    public void setComplexity(Complexity selectedComplexity){
+        this.selectedComplexity = selectedComplexity;
+        setSumOfDomVal();
+    }
+
+    //Misc. Methods
 }
