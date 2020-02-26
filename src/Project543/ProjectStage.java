a82a410d6af10073edba275667e1a5c210aabe97
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-02-26 15:17:16 -0800

    Added

--- src/Project543/ProjectStage.java
+++ src/Project543/ProjectStage.java
@@ -1,13 +1,22 @@
 package Project543;
 
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
 
 public class ProjectStage extends Stage {
+    public enum MenuText {
+        FILE, EDIT, PREFERENCES, METRICS, HELP;
+    }
+
     //Member Variables
     public static final int DEF_WINDOW_HEIGHT = 775;
     public static final int DEF_WINDOW_WIDTH = 800;
     public static final int DEF_X_POS = 100;
     public static final int DEF_Y_POS = 100;
+
+    Menu menuList[];
+    Scene windowScene;
 
     //Member Methods
 
