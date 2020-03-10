9d4cf6567c3ac421ab64b7d1a54c605df432526b
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-03-10 09:25:57 -0700

    Introduced UserInterface.java for next project iteration

---
+++ src/Project543/UserInterface.java
@@ -0,0 +1,67 @@
+//User Interface class for revision two of the project.
+//
+//NOT YET COMPLETE NOR INTENDED FOR USE AT THIS MOMENT
+
+package Project543;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+public class UserInterface {
+    //Member Variables
+    //
+
+    //Member Methods
+    //
+    //Constructor(s)
+    //
+
+    //Getters
+    //
+
+    //Setters
+    //
+
+    //Misc. Member Methods
+    //
+    public static void openMainWindow(Controller controller){
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("CECS 543 Metrics Suite");
+        primaryStage.setX(0); primaryStage.setX(0); primaryStage.setWidth(800); primaryStage.setHeight(775);
+        //Scene primaryScene = new Scene(primaryStage, 800, 775);
+
+        //Create the Menu Bar for the main menu
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        Menu preferencesMenu = new Menu("Preferences");
+        Menu metricsMenu = new Menu("Metrics");
+        menuBar.getMenus().addAll(fileMenu, editMenu, preferencesMenu, metricsMenu);
+
+        //Fill "File"
+        MenuItem[] fileMenuList = new MenuItem[4];
+        fileMenuList[0] = new MenuItem("New");
+        fileMenuList[1] = new MenuItem("Open");
+        fileMenuList[2] = new MenuItem("Save");
+        fileMenuList[3] = new MenuItem("Exit");
+
+        //Set fileMenuList actions
+    }
+
+    public static void openProjectPane(ProjectData project){
+
+    }
+
+    public static void createNewProject(ProjectData newProject){
+        //Open new project dialog window
+        //Set newProject data to values in dialog window
+        openProjectPane(newProject);
+    }
+
+    public static void openLanguageSelectionWindow(ProjectData project){
+
+    }
+}
