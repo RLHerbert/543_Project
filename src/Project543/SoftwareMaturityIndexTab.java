30c7a424dfbb50965838c2ef6f0fbbcdc9164d1e
Melissa Hazlewood <meli.hazlewood@gmail.com>
2020-03-20 11:25:15 -0700

    Prototypes for smi tab

--- src/Project543/SoftwareMaturityIndexTab.java
+++ src/Project543/SoftwareMaturityIndexTab.java
@@ -1,9 +1,12 @@
 package Project543;
 
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 
 //TODO
-public class SoftwareMaturityIndexTab extends Tab {
+public class SoftwareMaturityIndexTab extends MetricsTab {
     //Member Fields
     //
     //Member Enums and Classes
@@ -13,6 +16,7 @@ public class SoftwareMaturityIndexTab extends Tab {
     //
     //Constant Static Fields
     //
+    public static final String TAB_TITLE = "SMI";
 
     //Non-Constant Static Fields
     //
@@ -24,17 +28,54 @@ public class SoftwareMaturityIndexTab extends Tab {
 
     //Non-Constant Member Fields
     //
+    SoftwareMaturityIndex softwareMaturityIndex;
+    TableView table = new TableView();
 
     //Member Methods
     //
     //Constructor(s)
     //
+    SoftwareMaturityIndexTab(String tabTitle){
+
+    }
+
+    SoftwareMaturityIndexTab(String tabTitle, SoftwareMaturityIndex softwareMaturityIndex){
+
+    }
+
+    SoftwareMaturityIndexTab(String tabTitle, String saveData){
+
+    }
 
     //Getters
     //
+    public int getModulesAddedFromInput(){
+        return 0;
+    }
+
+    public int getModulesChangedFromInput(){
+        return 0;
+    }
+
+    public int getModulesDeletedFromInput(){
+        return 0;
+    }
 
     //Setters
     //
+    public void setTable(){
+
+    }
+
+    //Setters from Parent Class (Defining Virtual Methods)
+    //
+    public void setMetric(){
+        this.metric = this.softwareMaturityIndex;
+    }
+
+    public void setTabTitle(){
+        this.setText(TAB_TITLE);
+    }
 
     //Misc. Member Methods
     //
