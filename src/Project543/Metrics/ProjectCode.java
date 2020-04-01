01bb6a0c6048cd4c437001efdb5278764c06d785
Melissa Hazlewood <meli.hazlewood@gmail.com>
2020-04-01 11:14:37 -0700

    added javajava files

--- src/Project543/Metrics/ProjectCode.java
+++ src/Project543/Metrics/ProjectCode.java
@@ -1,4 +1,45 @@
 package Project543.Metrics;
 
 public class ProjectCode {
+    //**MEMBER FIELDS**//
+    //
+    //MEMBER ENUMS AND CLASSES
+    //
+
+    //STATIC MEMBER FIELDS
+    //
+    //Constant Static Fields
+    //
+
+    //Non-Constant Static Fields
+    //
+
+    //NON-STATIC MEMBER FIELDS
+    //
+    //Constant Member Fields
+    //
+
+    //Non-Constant Member Fields
+    //
+
+    //**MEMBER METHODS**//
+    //
+    //CONSTRUCTOR(S)
+    //
+
+    //GETTERS
+    //
+
+    //SETTERS
+    //
+
+    //MISC. MEMBER METHODS
+    //
+    //readFile
+    //do antlr stuff
+    //string that returns what antlr pops out
+    //hold file name/path/address
+    //read out save data arraylist of project save data - metrid id, file path
+    //metric id
+    //extends metrics
 }
