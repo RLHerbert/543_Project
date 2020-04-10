bbfd90da27f8eb095007cbbfdeb765dceef95d41
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-04-10 14:45:56 -0700

    First iteration of Dialogs class

--- src/Project543/Interface/Dialogs.java
+++ src/Project543/Interface/Dialogs.java
@@ -1,4 +1,20 @@
 package Project543.Interface;
 
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
+
 public class Dialogs {
+    public static String createFunctionPointDialog(){
+        String tabName = "Function Point";
+
+        TextInputDialog functionPointDialog = new TextInputDialog(tabName);
+        functionPointDialog.setTitle("New Function Point");
+        functionPointDialog.setHeaderText(null);
+        functionPointDialog.setContentText("Enter a name for the function point:");
+
+        Optional<String> result = functionPointDialog.showAndWait();
+
+        return result.orElse(null);
+    }
 }
