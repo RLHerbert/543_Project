2467032abf0f15652fb2bd1c58b8366d2cdbbbd2
RhymesWithCow <37909949+RhymesWithCow@users.noreply.github.com>
2020-04-12 11:29:53 -0700

    Refactored UI_3

--- src/Project543/TestBench.java
+++ src/Project543/TestBench.java
@@ -1,12 +1,6 @@
 package Project543;
 
 import Project543.Metrics.ProjectCode;
-import Project543.MetricsInterface.ProjectCodeTab;
-import javafx.scene.Scene;
-import javafx.scene.control.ChoiceDialog;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.antlr.runtime.RecognitionException;
 
@@ -18,7 +12,7 @@ public class TestBench {
     public TestBench(){
         //Things to test
 
-        Stage ui3 = new UserInterface_3();
+        Stage ui3 = new ProjectWindow();
 
 
         System.out.println("Constructed TestBench");
